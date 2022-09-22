8647e0ce75baadc7e27ce59cec97d02028c99e8b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-09-22 20:43:55 +0330

    Import sample data at home page Fixes #2

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,28 +5,40 @@ import Modal from "../components/common/Modal";
 import sampleData from "../data/sampleData.json";
 
 const Settings = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [importModalIsOpen, setImportModalIsOpen] = useState(false);
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
 
   const handleImport = () => {
     localStorage.removeItem("transactions");
     localStorage.setItem("transactions", JSON.stringify(sampleData));
-    setModalIsOpen(false);
+    setImportModalIsOpen(false);
+  };
+
+  const handleDeleteAll = () => {
+    localStorage.removeItem("transactions");
+    setDeleteModalIsOpen(false);
   };
 
   return (
     <>
-      <div className="p-4">
+      <div className="flex flex-col p-4 gap-4">
         <button
           className="bg-gray-500 text-white px-4 py-2 rounded-md"
-          onClick={() => setModalIsOpen(true)}
+          onClick={() => setImportModalIsOpen(true)}
         >
           Import Sample Data
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md"
+          onClick={() => setDeleteModalIsOpen(true)}
+        >
+          Remove All Data
         </button>
       </div>
 
       <Modal
-        isOpen={modalIsOpen}
-        setIsOpen={setModalIsOpen}
+        isOpen={importModalIsOpen}
+        setIsOpen={setImportModalIsOpen}
         rightButtonText="Import"
         rightButtonClassName="bg-red-500"
         onSubmit={handleImport}
@@ -34,6 +46,16 @@ const Settings = () => {
         <p className="text-red-500">
           All previous data will be replaced by the sample data. Are you sure?
         </p>
+      </Modal>
+
+      <Modal
+        isOpen={deleteModalIsOpen}
+        setIsOpen={setDeleteModalIsOpen}
+        rightButtonText="Delete all"
+        rightButtonClassName="bg-red-500"
+        onSubmit={handleDeleteAll}
+      >
+        <p className="text-red-500">Are you sure want to delete all data?</p>
       </Modal>
     </>
   );
