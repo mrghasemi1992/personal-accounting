8e78a48f973e8e5828c185d1636a41e3f3b5f04b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-09-23 12:55:28 +0330

    Reports Fixes #1

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { PieChart, Pie, Sector, ResponsiveContainer } from "recharts";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 import _ from "underscore";
+
 import { Transaction } from "../interfaces";
 
 const Reports = () => {
-  const [state, setState] = useState({
-    activeIndex: 0,
-  });
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
   const transactions: Transaction[] = JSON.parse(
     localStorage.getItem("transactions") || ""
   );
@@ -15,24 +15,64 @@ const Reports = () => {
     _.groupBy(transactions, (el) => el.category)
   );
 
-  const data = groupByCategory.map((item) => {
+  let data = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      },
+    ],
+  };
+
+  groupByCategory.forEach((item) => {
     let totalPrice = 0;
     item[1].forEach((transaction) => (totalPrice += Number(transaction.price)));
-    return { name: item[0], value: totalPrice };
+    // @ts-ignore
+    data.labels.push(item[0]);
+    // @ts-ignore
+    data.datasets[0].data.push(totalPrice);
   });
 
   return (
-    <PieChart width={375} height={375} className="mx-auto">
-      <Pie
-        data={data}
-        cx="50%"
-        cy="50%"
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      />
-    </PieChart>
+    <div>
+      <div className="flex gap-x-4 justify-center overflow-x-scroll mb-8 bg-gray-500 text-white px-4 py-2">
+        {[
+          // "April",
+          // "May",
+          // "June",
+          // "July",
+          // "August",
+          // "September",
+          // "Prev Month",
+          // "This Month",
+          "Total",
+        ].map((item, index) => (
+          <p
+            className={`${
+              index === 0 ? "border-b-2 border-white " : ""
+            }whitespace-nowrap`}
+            key={index}
+          >
+            {item}
+          </p>
+        ))}
+      </div>
+      <div className="max-w-xs mx-auto">
+        <Doughnut data={data} />
+      </div>
+      <hr className="my-8" />
+      {data.labels.map((label, index) => (
+        <div
+          key={index}
+          className="flex justify-between max-w-xs mx-auto mb-2"
+          style={{ color: data.datasets[0].backgroundColor[index] }}
+        >
+          <p>{label}</p>
+          <p>${data.datasets[0].data[index]}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
